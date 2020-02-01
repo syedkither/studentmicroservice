7c1f502adcfe44f04fcb7a7d5525abbd60476df0
syed <syedrifai81@gmail.com>
2020-02-01 16:24:24 -0600

    added

--- src/main/java/student/SwaggerConfig.java
+++ src/main/java/student/SwaggerConfig.java
@@ -11,6 +11,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -38,7 +39,14 @@ public class SwaggerConfig {
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("Student Register API")
+		Contact contact = new Contact(
+	               "Syed Kither",
+	               "https://github.com/syedkither", 
+	               "syedrifai81@gmail.com"
+	       );
+		return new ApiInfoBuilder().title("Student Register API").contact(contact)
+				.license("Apache 2.0")
+				.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
 				.description("This is the mock Library API can be used to practise the swagger documentation")
 				.version("1.0.0").build();
 	}   
